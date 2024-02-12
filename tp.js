9c1c1d9b5490e42fdedb22ff653a869ee9f55f7d
redaguermellou <55282065+redaguermellou@users.noreply.github.com>
2024-02-12 21:56:30 +0100

    Add files via upload

---
+++ tp.js
@@ -0,0 +1,43 @@
+let x = 1; 
+let y = 2;
+let somme = x+y;
+let diference = x-y;
+let produit = x*y;
+let quotion = x/y;
+console.log(somme);
+console.log(diference);
+console.log(produit);
+console.log(quotion);
+
+
+let age = 18;
+if(age<=18){
+    console.log('majeur');
+
+}else{
+    console.log('mineur');
+}
+
+
+let tab =['orange','pomme','banana'];
+for(let i=0;i<3;i++){
+    console.log(tab[i]);
+
+}
+
+
+somme=function (x,y){
+    console.log(x+y);
+}
+
+somme(2,3);
+
+let personne = {nom:'reda',age:18,ville:'casablanca'};
+console.log(personne);
+
+
+
+
+fruit.forEach(element => {
+    
+});
