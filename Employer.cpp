3255c58da4c3e49fa93a4e9af9232b9464f629df
reda guermellou <55282065+redaguermellou@users.noreply.github.com>
2024-11-10 13:31:09 +0100

    Update Employer.cpp

--- Employer.cpp
+++ Employer.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include "employer.h"
+using namespace std;
 
+
+void employer::afficherdetaille() {
+    cout << this->id << ", " << this->nom << ", " << this->salaire << endl;
+}
+
+
+float employer::augmentersalaire(int pourcentage) {
+    float salaire1 = this->salaire * (pourcentage / 100.0); 
+    this->salaire += salaire1; 
+    return this->salaire;
+}
